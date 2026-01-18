78dcad330e98cd9a4c232cc1eec5e6e0cfdf3446
berrksancakk <kasimberksancak61@gmail.com>
2026-01-18 19:45:01 +0300

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,39 +1,63 @@
 # -*- coding: utf-8 -*-
-"""
-Temel sinyal analiz fonksiyonları
-"""
+#"""
+#Temel sinyal analiz fonksiyonları
+#"""
 
-def sampling_rate(t):
-    """
-    Zaman vektöründen örnekleme frekansını hesaplar
+#def sampling_rate(t):
+#    """
+#    Zaman vektöründen örnekleme frekansını hesaplar
 
-    Parametre:
-        t : zaman vektörü
+#    Parametre:
+#        t : zaman vektörü
 
-    Dönen:
-        fs : örnekleme frekansı (Hz)
-    """
+#    Dönen:
+#        fs : örnekleme frekansı (Hz)
+#    """
     # TODO:
     # 1. ardışık iki zaman örneği arasındaki farkı hesapla
     # 2. fs = 1 / dt
-    pass
+#    pass
+t = np.array([0, 0.01, 0.02, 0.03])
+x = np.array([1, 2, 3, 4])
 
+    import numpy as np
 
-def basic_stats(x):
-    """
-    Sinyal için temel istatistikleri hesaplar
+def sampling_rate(t):
+    # Ardışık iki zaman örneği arasındaki fark
+    dt = t[1] - t[0]
 
-    Parametre:
-        x : sinyal vektörü
+    # Örnekleme frekansı
+    fs = 1 / dt
 
-    Dönen:
-        stats (dict):
-            mean
-            std
-            rms
-            min
-            max
-    """
+    return fs
+
+#def basic_stats(x):
+#    """
+#    Sinyal için temel istatistikleri hesaplar
+
+#    Parametre:
+#        x : sinyal vektörü
+
+#    Dönen:
+#        stats (dict):
+#            mean
+#            std
+#            rms
+#            min
+#            max
+#    """
     # TODO:
     # numpy kullanarak gerekli istatistikleri hesapla
-    pass
+#    pass
+#    import numpy as np
+
+def basic_stats(x):
+    stats = {}
+
+    stats["mean"] = np.mean(x)
+    stats["std"] = np.std(x)
+    stats["rms"] = np.sqrt(np.mean(x**2))
+    stats["min"] = np.min(x)
+    stats["max"] = np.max(x)
+
+    return stats
